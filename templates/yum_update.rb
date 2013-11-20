77f5a91fde19bb804b8955173af1c0b72be2c9bf
kashif74 <>
2013-11-20 13:49:51 +0000

    remove typo

--- templates/yum_update.rb
+++ templates/yum_update.rb
@@ -1,4 +1,4 @@
 #!/bin/bash
 /bin/sleep `expr $RANDOM % 1000`
 
-/usr/bin/yum -y  update --disablerepo=*  <% @enable.each do |val| -%> --enablerepo=<%=val -%> <% end -%>  <% @exclude.each do |val1| -%> --exclude=<%=val1 -%> <% end -%>/usr/bin/yum -y  update --disablerepo=*  <% @enable.each do |val| -%> --enablerepo=<%=val -%> <% end -%>  <% @exclude.each do |val1| -%> --exclude=<%=val1 -%> <% end -%>
+/usr/bin/yum -y  update --disablerepo=*  <% @enable.each do |val| -%> --enablerepo=<%=val -%> <% end -%>  <% @exclude.each do |val1| -%> --exclude=<%=val1 -%> <% end -%>
